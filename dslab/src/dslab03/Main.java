48308c53e3150cd10f44f09b15aa3375dae2aa0d
pitiz29a@gmail.com <pitiz29a@gmail.com@f7ec7cca-6b9c-bc99-6956-44bd4127b7e6>
2013-03-25 13:16:27 +0000

    Priority Queue included!

--- dslab/src/dslab03/Main.java
+++ dslab/src/dslab03/Main.java
@@ -1,14 +1,31 @@
 package dslab03;
 
+import java.util.Random;
+
 public class Main {
 	
-	private static final int NUMBEROFCLIENTS = 200;
-	private static Office registryOffice = new Office(15);
+	private static final int MAXOFFICECAPACITY = 10;
+	private static final int BRANCHCAPACITY = 3;
+	private static final int EMPLOYEECAPACITY = 1;
+	private static final int NUMBEROFCLIENTS = 1000;
+	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+
+		Office[] employee1 = {new Office(EMPLOYEECAPACITY, "Employee1")};
+		Office[] employee2 = {new Office(EMPLOYEECAPACITY, "Employee2")};
+		Office[] employee3 = {new Office(EMPLOYEECAPACITY, "Employee3")};
+	 	
+		Office[] branches = {
+				new Office(BRANCHCAPACITY, "Branch1 - Queue", employee1),
+				new Office(BRANCHCAPACITY, "Branch2 - Queue", employee2),
+				new Office(BRANCHCAPACITY, "Branch3 - Queue", employee3)};
+	 	
+		Office registryOffice = new Office(MAXOFFICECAPACITY, "Registry Office", branches);
+		registryOffice.setPrioritizedQueue(true);
 		
 		for(int i = 0; i < NUMBEROFCLIENTS; i++) {
-			Thread t = new Citizen(registryOffice);
+			Thread t = new Citizen(registryOffice, i % 2 == 0 ? true : false, (new Random()).nextInt(50) + 18);
 			t.start();
 		}
 	}
--- dslab/src/dslab03/Office.java
+++ dslab/src/dslab03/Office.java
@@ -1,90 +1,111 @@
 package dslab03;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-import javax.swing.text.html.MinimalHTMLWriter;
-
 public class Office {
 	
 	private int maxCapacity;
-	private final ArrayList<Citizen> branch1 = new ArrayList<Citizen>(); 
-	private final ArrayList<Citizen> branch2 = new ArrayList<Citizen>(); 
-	private final ArrayList<Citizen> branch3 = new ArrayList<Citizen>(); 
+	private String name;
+	private final ArrayList<Citizen> citizen = new ArrayList<Citizen>();
+	private Office[] branches = null;
 	private final Lock lock = new ReentrantLock(); 
 	private final Condition entryPermit = lock.newCondition(); 
-	private final Condition leavePermit = lock.newCondition(); 
+	private final Condition leavePermit = lock.newCondition();
+	private boolean isPrioritizedQueue = false; 
  
-	public Office(int maxCapacity) {
+	public Office(int maxCapacity, String name, Office[] branches) throws Exception {
 		this.maxCapacity = maxCapacity;
-	}
-	
-	private int personsInOffice() {
-		return branch1.size() + branch2.size() + branch3.size();
-	}
-	
-	private ArrayList<Citizen> shortestLine() {
-		int[] branches = {branch1.size(), branch2.size(), branch3.size()};
-		Arrays.sort(branches);
-		int length = branches[0];
-		if(length == branch1.size()) {
-			return branch1;
+		this.branches = new Office[branches.length];
+		for(int i = 0; i < branches.length; i++) {
+			this.branches[i] = branches[i];
 		}
-		if(length == branch2.size()) {
-			return branch2;
-		}
-		return branch3;
-	}
-	
-	private ArrayList<Citizen> add(Citizen newCitizen) {
-		ArrayList<Citizen> branch = shortestLine();
-		branch.add(newCitizen);
-		return branch;
-	}
-	
-	private void remove(Citizen citizen) {
-		try {
-			branch1.remove(citizen);
-		} catch (Exception e) {
-		}
-		try {
-			branch2.remove(citizen);
-		} catch (Exception e) {
-		}
-		try {
-			branch3.remove(citizen);
-		} catch (Exception e) {
-		}
+		this.name = name;
 	}
 
-	public void enterRoom(Citizen newCitizen) throws InterruptedException { 
+	public Office(int maxCapacity, String name) {
+		this.maxCapacity = maxCapacity;
+		this.name = name;
+	}
+	
+	public void setPrioritizedQueue(boolean pr) {
+		isPrioritizedQueue = pr;
+	}
+
+	public int countCitizen() {
+		return citizen.size();
+	}
+	
+	private int shortestLineId() {
+		if(branches == null) {
+			return -1;
+		}
+		int shortest = 0;
+		for(int i = 1; i < branches.length; i++) {
+			if(branches[i].countCitizen() < branches[i-1].countCitizen()) {
+				shortest = i;
+			}
+		}
+		return shortest;
+	}
+	
+	public boolean hasCitizen(Citizen c) {
+		return citizen.contains(c);
+	}
+	
+	public boolean canEnter(Citizen c) {
+		if(isPrioritizedQueue ) {
+			Collections.sort(citizen);
+			if(citizen != null && citizen.size() > 0 && citizen.get(0).compareTo(c) < 0) {
+				System.out.println(c + " has the hightest priority in the waiting queue for " + name);
+				return true;
+			}
+		}
+		if(countCitizen() < maxCapacity) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void enterRoom(Citizen c) throws InterruptedException { 
 	    lock.lock(); 
 	    try { 
-	    	while (personsInOffice() == maxCapacity) { 
-	    		System.out.println(newCitizen + " waits to enter the room!");
+	    	while (!canEnter(c)) { 
+	    		System.out.println(c + " waits to enter " + name);
 	    		entryPermit.await(); 
 	    	} 
-	        add(newCitizen); 
-	        System.out.println(newCitizen + " entered the room! Count = " + personsInOffice());
-	        leavePermit.signal(); 
+	        citizen.add(c); 
+	        System.out.println(c + " entered " + name + "; Count = " + countCitizen());
+	        leavePermit.signalAll(); 
 	    } finally { 
 	    	lock.unlock(); 
 	    } 
+        if(branches != null) {
+        	branches[shortestLineId()].enterRoom(c);
+        }
 	} 
 	
-	public void leaveRoom(Citizen newCitizen) throws InterruptedException {
+	public void leaveRoom(Citizen c) throws InterruptedException {
+		if(branches != null) {
+			int i = 0;
+			while(i < branches.length && !branches[i].hasCitizen(c)) {
+				i++;
+			}
+			branches[i].leaveRoom(c);
+		}
+
 		lock.lock();
 		try {
-			while(personsInOffice() == 0) {
-	    		System.out.println(newCitizen + " waits to leave the room!");
+			while(countCitizen() == 0) {
+	    		System.out.println(c + " waits to leave " + name);
 				leavePermit.await();
 			}
-			remove(newCitizen);
-	        System.out.println(newCitizen + " left the room! Count = " + personsInOffice());
-			entryPermit.signal();
+			citizen.remove(c);
+	        System.out.println(c + " left " + name + "; Count = " + countCitizen());
+			entryPermit.signalAll();
 		} finally {
 			lock.unlock();
 		}
