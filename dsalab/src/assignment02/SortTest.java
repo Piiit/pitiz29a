aec43f8d4d5b3000c81d387719231ab2bd77ed98
pitiz29a@gmail.com <pitiz29a@gmail.com@f7ec7cca-6b9c-bc99-6956-44bd4127b7e6>
2013-03-25 17:44:30 +0000

    DSA - Full tests...

--- dsalab/src/assignment02/SortTest.java
+++ dsalab/src/assignment02/SortTest.java
@@ -33,7 +33,7 @@ public class SortTest {
 		System.out.println(Arrays.toString(array4));
 		
 		
-//		int[] A;
+		int[] A;
 		double[] estimatesInsertion = new double[REPEATS];
 		double[] estimatesMerge = new double[REPEATS];
 		int i = 0;
@@ -41,46 +41,14 @@ public class SortTest {
 		double estimatedTimeMergeSort = 0;
 		long startTime = 0;
 		
-//		try {
-//			String formatTitle = "%-15s|%-11s|%-22s|%-22s|%-24s|%s\n";
-//			String format = "%15s|%11s|%22.2f|%22.2f|%24.2f|%s\n";
-//			System.out.printf(formatTitle, "No. of Elements", "No. of Runs", "Insertion Sort Median", "Merge Sort Median", "Winner is x times faster", "Winner");
-//			while(estimatedTimeInsertionSort < 5000000000L && estimatedTimeMergeSort < 5000000000L) {
-//				i++;
-//				for(int times = 0; times < REPEATS; times++) {
-//					A = ArrayUtility.createRandomArray((int)Math.pow(2, i), 0, 1000);
-//					
-//					startTime = System.nanoTime();
-//					InsertionSort.sort(A);
-//				    estimatesInsertion[times] = System.nanoTime() - startTime;
-//
-//				    startTime = System.nanoTime();
-//					MergeSort.sort(A);
-//				    estimatesMerge[times] = System.nanoTime() - startTime;
-//				}
-//				estimatedTimeInsertionSort = ArrayUtility.median(estimatesInsertion);
-//				estimatedTimeMergeSort = ArrayUtility.median(estimatesMerge);
-//				System.out.printf(format, 
-//						(int)Math.pow(2, i), 
-//						REPEATS, 
-//						estimatedTimeInsertionSort, 
-//						estimatedTimeMergeSort, 
-//						(estimatedTimeInsertionSort > estimatedTimeMergeSort ? estimatedTimeInsertionSort/estimatedTimeMergeSort: estimatedTimeMergeSort/estimatedTimeInsertionSort),
-//						(estimatedTimeInsertionSort > estimatedTimeMergeSort ? "MergeSort" : "InsertionSort") + " wins!");
-//			}
-//		} catch (Exception e1) {
-//			e1.printStackTrace();
-//		}	
-		
-		BigInteger[] A;
 		try {
 			String formatTitle = "%-15s|%-11s|%-22s|%-22s|%-24s|%s\n";
 			String format = "%15s|%11s|%22.2f|%22.2f|%24.2f|%s\n";
 			System.out.printf(formatTitle, "No. of Elements", "No. of Runs", "Insertion Sort Median", "Merge Sort Median", "Winner is x times faster", "Winner");
-			while(estimatedTimeInsertionSort < 5000000000L && estimatedTimeMergeSort < 5000000000L) {
+			while(estimatedTimeInsertionSort < 50000000L && estimatedTimeMergeSort < 50000000L) {
 				i++;
 				for(int times = 0; times < REPEATS; times++) {
-					A = ArrayUtility.createRandomArrayBigInt((int)Math.pow(2, i), 2048);
+					A = ArrayUtility.createRandomArray((int)Math.pow(2, i), 0, 1000);
 					
 					startTime = System.nanoTime();
 					InsertionSort.sort(A);
@@ -88,6 +56,78 @@ public class SortTest {
 
 				    startTime = System.nanoTime();
 					MergeSort.sort(A);
+				    estimatesMerge[times] = System.nanoTime() - startTime;
+				}
+				estimatedTimeInsertionSort = ArrayUtility.median(estimatesInsertion);
+				estimatedTimeMergeSort = ArrayUtility.median(estimatesMerge);
+				System.out.printf(format, 
+						(int)Math.pow(2, i), 
+						REPEATS, 
+						estimatedTimeInsertionSort, 
+						estimatedTimeMergeSort, 
+						(estimatedTimeInsertionSort > estimatedTimeMergeSort ? estimatedTimeInsertionSort/estimatedTimeMergeSort: estimatedTimeMergeSort/estimatedTimeInsertionSort),
+						(estimatedTimeInsertionSort > estimatedTimeMergeSort ? "MergeSort" : "InsertionSort"));
+			}
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}	
+		
+		i = 0;
+		estimatedTimeInsertionSort = 0;
+		estimatedTimeMergeSort = 0;
+		startTime = 0;
+		BigInteger[] B;
+		try {
+			String formatTitle = "%-15s|%-11s|%-22s|%-22s|%-24s|%s\n";
+			String format = "%15s|%11s|%22.2f|%22.2f|%24.2f|%s\n";
+			System.out.printf(formatTitle, "No. of Elements", "No. of Runs", "Insertion Sort Median", "Merge Sort Median", "Winner is x times faster", "Winner");
+			while(estimatedTimeInsertionSort < 50000000L && estimatedTimeMergeSort < 50000000L) {
+				i++;
+				for(int times = 0; times < REPEATS; times++) {
+					B = ArrayUtility.createRandomArrayBigInt((int)Math.pow(2, i), 1024);
+					
+					startTime = System.nanoTime();
+					InsertionSort.sort(B);
+				    estimatesInsertion[times] = System.nanoTime() - startTime;
+
+				    startTime = System.nanoTime();
+					MergeSort.sort(B);
+				    estimatesMerge[times] = System.nanoTime() - startTime;
+				}
+				estimatedTimeInsertionSort = ArrayUtility.median(estimatesInsertion);
+				estimatedTimeMergeSort = ArrayUtility.median(estimatesMerge);
+				System.out.printf(format, 
+						(int)Math.pow(2, i), 
+						REPEATS, 
+						estimatedTimeInsertionSort, 
+						estimatedTimeMergeSort, 
+						(estimatedTimeInsertionSort > estimatedTimeMergeSort ? estimatedTimeInsertionSort/estimatedTimeMergeSort: estimatedTimeMergeSort/estimatedTimeInsertionSort),
+						(estimatedTimeInsertionSort > estimatedTimeMergeSort ? "MergeSort" : "InsertionSort"));
+			}
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}	
+		
+		i = 0;
+		estimatedTimeInsertionSort = 0;
+		estimatedTimeMergeSort = 0;
+		startTime = 0;
+		B = null;
+		try {
+			String formatTitle = "%-15s|%-11s|%-22s|%-22s|%-24s|%s\n";
+			String format = "%15s|%11s|%22.2f|%22.2f|%24.2f|%s\n";
+			System.out.printf(formatTitle, "No. of Elements", "No. of Runs", "Insertion Sort Median", "Merge Sort Median", "Winner is x times faster", "Winner");
+			while(estimatedTimeInsertionSort < 50000000L && estimatedTimeMergeSort < 50000000L) {
+				i++;
+				for(int times = 0; times < REPEATS; times++) {
+					B = ArrayUtility.createRandomArrayBigInt((int)Math.pow(2, i), 2048);
+					
+					startTime = System.nanoTime();
+					InsertionSort.sort(B);
+				    estimatesInsertion[times] = System.nanoTime() - startTime;
+
+				    startTime = System.nanoTime();
+					MergeSort.sort(B);
 				    estimatesMerge[times] = System.nanoTime() - startTime;
 				}
 				estimatedTimeInsertionSort = ArrayUtility.median(estimatesInsertion);
