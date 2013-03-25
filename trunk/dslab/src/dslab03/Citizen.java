b7541739c3d9ec4f3c6cdf55e64a9c1031e06326
pitiz29a@gmail.com <pitiz29a@gmail.com@f7ec7cca-6b9c-bc99-6956-44bd4127b7e6>
2013-03-25 13:16:27 +0000

    Priority Queue included!

--- trunk/dslab/src/dslab03/Citizen.java
+++ trunk/dslab/src/dslab03/Citizen.java
@@ -2,20 +2,24 @@ package dslab03;
 
 import java.util.Random;
 
-public class Citizen extends Thread {
+public class Citizen extends Thread implements Comparable<Citizen> {
 	
 	private Office office;
+	private boolean isMale;
+	private int age = 30;
 	
-    public Citizen(Office office) {
+    public Citizen(Office office, boolean isMale, int age) {
 		this.office = office;
+		this.isMale = isMale;
+		this.age = age;
 	}
 
 	public void run() {
-//    	System.out.println(this + " arrived!");
+    	System.out.println(this + " arrived!");
     	try {
 			Thread.sleep((new Random()).nextInt(1000));
 			office.enterRoom(this);
-			Thread.sleep((new Random()).nextInt(5000));
+			Thread.sleep((new Random()).nextInt(2000) + 1000);
 			office.leaveRoom(this);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -24,7 +28,18 @@ public class Citizen extends Thread {
 
 	@Override
 	public String toString() {
-		return "Citizen " + getId();
+		return age + " year old " + (isMale ? "male" : "female") + " citizen with id " + getId();
+	}
+
+	@Override
+	public int compareTo(Citizen o) {
+		if(age < o.age) {
+			return -1;
+		} 
+		if(age > o.age) {
+			return 1;
+		}
+		return 0;
 	}
 	
 	
