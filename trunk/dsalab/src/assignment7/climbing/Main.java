80da57fc53145636b2a60651b63025d824d08e9f
pitiz29a@gmail.com <pitiz29a@gmail.com@f7ec7cca-6b9c-bc99-6956-44bd4127b7e6>
2013-06-07 15:02:47 +0000

    Measurement of climbing algorithms

--- trunk/dsalab/src/assignment7/climbing/Main.java
+++ trunk/dsalab/src/assignment7/climbing/Main.java
@@ -7,15 +7,29 @@ public class Main {
 		System.out.println("This is the recursive solution...");
 		Wall wall1 = new Wall();
 		Climbing cr1 = new Climbing(wall1);
+		long startTime = System.nanoTime();
 		ClimbingNode root1 = cr1.getBestRouteRec();
+		long runningTime = (System.nanoTime() - startTime);
+		System.out.println("Running time = " + runningTime);
 		root1.printPath();
 		
 		System.out.println();
 		System.out.println("...and this is the dynamic programming solution... (ordering reversed)");
 		Wall wall2 = new Wall();
 		Climbing cr2 = new Climbing(wall2);
+		long startTime2 = System.nanoTime();
 		ClimbingNode root2 = cr2.getBestRouteDP();
+		long runningTime2 = (System.nanoTime() - startTime2);
+		System.out.println("Running time = " + runningTime2);
 		root2.printPath();
+		
+		System.out.println();
+		if(runningTime < runningTime2) {
+			System.out.println("The recursive algorithm was " + (float)runningTime/runningTime2+ " times faster than the DP-algorithm.");
+		} else {
+			System.out.println("The DP-algorithm was " + (float)runningTime/runningTime2 + " times faster than the recursive algorithm.");
+			
+		}
 	}
 
 }
